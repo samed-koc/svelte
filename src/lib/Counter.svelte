d5f00a91002e61e9fdb05bc0d82356e1f94418af
samed-koc <k.kartal9595@gmail.com>
2026-03-03 01:37:49 +0100

    added:one script can two funktion call

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -3,8 +3,18 @@
   const increment = () => {
     count += 1
   }
+
+  let counter = $state(0)
+  const incrementCounter = () => {
+    counter += 2
+  }
+
 </script>
 
-<button onclick={increment}>
-  count is {count}
+
+
+<button onclick={incrementCounter}>
+  count is {counter}
 </button>
+
+<button onclick={increment}>{count}</button>
